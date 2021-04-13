a9431fe2fb3a87f998d88e00571b286894e3e856
Bruno <bruno.umbelino0@gmail.com>
2021-04-12 22:59:15 -0300

    first commit

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";document.body.appendChild((()=>{const e=document.createElement("h1");return e.innerHTML="Boa noite.",e.classList.add("title"),e})())})();
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          // Creates `style` nodes from JS strings
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "login.html",
+      template: "./src/login.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+  ],
+};
